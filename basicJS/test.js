2f4f97e71fa23a9d7221a49d40fa4560f9a16644
Wolf1610 <dutta2772004@gmail.com>
2024-07-22 14:45:55 +0000

    Data_type

--- basicJS/test.js
+++ basicJS/test.js
@@ -1,1 +1,20 @@
 console.log("Hello")
+
+
+const accountId = 1453223
+let accountEmail = "wolf@gmail.com"
+var accountPassword = "1234" // don't use
+accountCity = "Hyderabad" // this is not a good way to declare stringz
+
+
+console.log(accountEmail)
+
+
+// 
+/*
+Prefer not to use var, because of issue in block scope and fuunctional scope
+
+const and let use, don't use var datatype
+*/
+
+console.table([accountId, accountEmail, accountPassword])
