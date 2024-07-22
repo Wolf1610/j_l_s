fb2a5d35d7c0a00dee01e6038c023003c05220ce
Wolf1610 <dutta2772004@gmail.com>
2024-07-22 15:16:03 +0000

    Conversion of Datatype

---
+++ basicJS/_3_conversionOperator.js
@@ -0,0 +1,26 @@
+let score = "54hgf"
+let score1 = null
+let score2 = undefined
+let score3 = true
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score1)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// If any number will not converting into number then, output is NaN
+
+/*
+"54" = 54
+"54dfg" = NaN
+true = 1
+false = 0
+*/
+
+// 1 -> true , 0 -> false
+// "" ->false
+// "hitesh" -> true
