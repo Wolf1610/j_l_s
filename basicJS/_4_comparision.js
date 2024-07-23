8cdc60a9edfa67d7d779c1865e8b55c3fcc3aab2
Wolf1610 <dutta2772004@gmail.com>
2024-07-23 14:28:13 +0000

    comparision of 2 operands including null and undefined

--- basicJS/_4_comparision.js
+++ basicJS/_4_comparision.js
@@ -3,8 +3,8 @@ console.log("3" > 2);
 console.log("03" > 1);
 
 
+// This is not a good practice to write this type of conversion below 
 console.log(null > 0);
-console.log(null == 0);
 console.log(null >= 0);
 console.log(null <= 0);
 
@@ -16,7 +16,7 @@ Comparision convert nnull to a number. treating it as 0.
 That's why null >= 0 is true and null > 0 is false
 */
 
-
+// This is not a good practice to write below 
 console.log(undefined == 0);
 console.log(undefined >= 0);
 console.log(undefined <= 0);
