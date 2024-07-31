f5fd5aaf48f2fc7a9593f4b2e7e511492aa44950
Wolf1610 <dutta2772004@gmail.com>
2024-07-31 13:59:20 +0000

    Obj destructure

---
+++ _2_basic/_5_obj_deStructure.js
@@ -0,0 +1,14 @@
+// DeStructure
+const course = {
+    coursename: "AWS",
+    price: "999",
+    courceInstructure: "Rishab"
+}
+
+// This is destructure
+const {courceInstructure: inst} = course;
+console.log(inst); 
+// Output: Rishab
+
+
+
